197c0914d627be0d31dfcb51903387a4bb8fa739
finalManu <manuelrydholm@gmail.com>
2023-08-08 11:31:08 +0200

    Part 1 Done

--- challenges/part1.js
+++ challenges/part1.js
@@ -1,1 +1,14 @@
 /** Write your code below */
+let post = "";
+jsInput = document.querySelector("input");
+jsDiv = document.querySelector("div");
+
+//another way to do it
+//jsInput.addEventListener("input", handleInput);
+
+jsInput.oninput = handleInput;
+
+function handleInput() {
+  post = jsInput.value;
+  jsDiv.textContent = post;
+}
