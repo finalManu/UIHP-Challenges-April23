815b93b70e24bd8180b6b16c25be7987c3b0a716
finalManu <manuelrydholm@gmail.com>
2023-08-13 13:03:39 +0200

    Part 4 (no ext)

--- challenges/part4.js
+++ challenges/part4.js
@@ -1,15 +1,62 @@
-let myName = "";
+// let myName = "";
 
-const vDOM = [
-  [
-    "input",
-    myName,
-    function handle() {
-      myName = jsInput.value;
-    },
-  ],
-  ["div", `Hello, ${myName}!`],
-];
+// const vDOM = [
+//   [
+//     "input",
+//     myName,
+//     function handle() {
+//       myName = jsInput.value;
+//     },
+//   ],
+//   ["div", `Hello, ${myName}!`],
+//   ["div", `Check it out now`],
+//   ["div", `The funk soul brother`],
+// ];
+
+// function convert(node) {
+//   const element = document.createElement(node[0]);
+//   element.textContent = node[1];
+//   element.value = node[1];
+//   element.oninput = node[2];
+//   return element;
+// }
+
+// const elems = vDOM.map(convert);
+// document.body.append(...elems);
+
+/** Step @todo */
+/* uncomment the code below this line, and comment out the code above*/
+
+let isFocus = false;
+
+let myName = "";
+let jsInput;
+let jsDiv;
+let vDOM;
+
+function createVDOM() {
+  return [
+    [
+      "input",
+      myName,
+      function handle(e) {
+        myName = e.target.value;
+      },
+    ],
+    ["div", `Hello, ${myName}!`],
+    ["div", `Como`],
+    ["div", `Estas?`],
+  ];
+}
+
+function updateDOM() {
+  document.activeElement === jsInput ? (isFocus = true) : (isFocus = false);
+  vDOM = createVDOM();
+  elems = vDOM.map(convert);
+  jsInput = elems[0];
+  document.body.replaceChildren(...elems);
+  if (isFocus) jsInput.focus();
+}
 
 function convert(node) {
   const element = document.createElement(node[0]);
@@ -19,44 +66,4 @@ function convert(node) {
   return element;
 }
 
-/** Step @todo */
-/* uncomment the code below this line, and comment out the code above*/
-
-// let isFocus = false
-
-// let myName = '';
-// let jsInput;
-// let jsDiv;
-// let vDOM;
-
-// function createVDOM() {
-//   return [
-//     [
-//       'input',
-//       myName,
-//       function handle() {
-//         myName = jsInput.value;
-//       },
-//     ],
-//     ['div', `Hello, ${myName}!`],
-//   ];
-// }
-
-// function updateDOM() {
-//   document.activeElement === jsInput ? (isFocus = true) : (isFocus = false);
-//   vDOM = createVDOM();
-//   jsInput = convert(vDOM[0]);
-//   jsDiv = convert(vDOM[1]);
-//   document.body.replaceChildren(jsInput, jsDiv);
-//   if(isFocus) jsInput.focus()
-// }
-
-// function convert(node) {
-//     const element = document.createElement(node[0]);
-//     element.textContent = node[1];
-//     element.value = node[1];
-//     element.oninput = node[2];
-//     return element;
-//   }
-
-// setInterval(updateDOM, 15);
+setInterval(updateDOM, 15);
